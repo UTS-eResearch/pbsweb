9212d938222a6ba031e08a9fa534f98e2e0345b5
Mike Lake <Mike.Lake@uts.edu.au>
2022-04-11 11:41:17 +1000

    Updated for python 3.

--- swig_compile_pbs.sh
+++ swig_compile_pbs.sh
@@ -3,17 +3,16 @@
 # This script uses the PBS supplied include file pbs_ifl.h (and pbs.i) 
 # to create pbs.py, pbs_wrap.c and _pbs.so.
 
-conf="/etc/pbs.conf"  # PBS configuration file
-
 #############################
 # Set your configuration here
 #############################
 
-# Example: If using a system installed python.
-PYTHON_INCL="/usr/include/python3.8"
+conf="/etc/pbs.conf"  # PBS configuration file
 
-# Example: If using Python in a virtual environment.
-#PYTHON_INCL=/var/www/wsgi/virtualenvs/pbsweb/include/python3.8
+# Specify here where the include files are for the version of Python that you are using.
+# If you are using a Python in a virtual environment then use the include location that 
+# the virtual env was derived from.
+PYTHON_INCL="/usr/include/python3.8"
 
 SWIG_EXEC="/usr/bin/swig"
 
@@ -28,6 +27,11 @@ fi
 # The PBS config file must be sourced to provide $PBS_EXEC.
 . $conf
 
+# Set LD_RUN_PATH here so users should not need to export LD_LIBRARY_PATH at run time.
+# If you link with libpbs.so below you will need to export LD_RUN_PATH.
+# If you link with libpbs.a then LD_RUN_PATH will not be needed.
+export LD_RUN_PATH=$PBS_EXEC/lib
+
 # Running swig creates pbs.py and pbs_wrap.c
 $SWIG_EXEC -I$PBS_EXEC/include -python pbs.i
 
@@ -36,14 +40,23 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-# If you link with libpbs.so below you will need to export LD_RUN_PATH.
-# If you link with libpbs.a then LD_RUN_PATH will not be needed.
-#export LD_RUN_PATH=$PBS_EXEC/lib
-
 # Running gcc creates _pbs.so
-gcc -shared -fPIC -I$PYTHON_INCL -I$PBS_EXEC/include pbs_wrap.c \
-    $PBS_EXEC/lib/libpbs.a \
-    -L/lib -lcrypto -lssl \
+# Note: This is what I had before, compile and link in one step. 
+# But I have commented out this and separated the compile and link
+# as its then easier to debug errors during build.
+#gcc -shared -fPIC -I$PYTHON_INCL -I$PBS_EXEC/include pbs_wrap.c \
+#    $PBS_EXEC/lib/libpbs.a \
+#    -L/lib -lpthread -lcrypto -lssl \
+#    -o _pbs.so 
+
+echo "compiling ..."
+gcc -c -shared -fpic -I$PYTHON_INCL -I$PBS_EXEC/include pbs_wrap.c # -o tmp.so 
+
+echo 'linking ...'
+#gcc -shared -fpic -L/lib -L/opt/pbs/lib \
+gcc -shared -fpic -L/opt/pbs/lib \
+    $PBS_EXEC/lib/libpbs.so pbs_wrap.o \
+    -lpthread -lcrypto -lssl -lsec \
     -o _pbs.so 
 
 # It does not need to be executable.
