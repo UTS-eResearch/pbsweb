6100fc468a6cfa8c9dce81d2358ae17341cf9a9e
Mike Lake <Mike.Lake@uts.edu.au>
2022-07-15 14:37:36 +1000

    Do not remove the pbs.py or .so

--- clean.sh
+++ clean.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Remove files created by SWIG or Python tests.
+# Remove intermediate files created by SWIG or Python tests,
+# and which are not needed at runtime.
 
-rm -f pbs.py
 rm -f pbs.pyc
 rm -f pbsutils.pyc
 rm -f pbs_wrap.c
 rm -f pbs_wrap.o
-rm -f _pbs.so
 rm -fr __pycache__
 
